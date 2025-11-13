ed774602e7cec663ed80bdf0e2ff3636ce327b8d
shuaibai <51265901062@stu.ecnu.edu.cn>
2025-11-13 22:42:39 +0800

    add the using.

---
+++ 10_using/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// using namespace::name
+using std::cin;
+using std::cout;
+using std::endl;
+int main() {
+    int i;
+    cin >> i;
+    cout << i << endl;
+    return 0;
+}
